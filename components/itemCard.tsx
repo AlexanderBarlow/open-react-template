8444b5b32dd4664a02f0346ba17e5d8cf554e2bf
alex barlow <alex.barlow@rocketmail.com>
2023-08-16 19:17:04 -0400

    gun page added styled cards

--- components/itemCard.tsx
+++ components/itemCard.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React, { use } from "react";
 import { useState, useEffect } from "react";
@@ -76,7 +76,7 @@ export default function itemCard() {
 
   useEffect(() => {
     if (posts.length > 0) {
-      const post:any = posts[0];
+      const post: any = posts[0];
       console.log(post);
       const traderSell = post.sellFor;
       console.log(traderSell);
@@ -147,8 +147,9 @@ export default function itemCard() {
                   >
                     <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
                   </svg>
-                  <span className="flex-1 ml-3 whitespace-nowrap">Assualt Rifles</span>
-                  
+                  <span className="flex-1 ml-3 whitespace-nowrap">
+                    Assualt Rifles
+                  </span>
                 </a>
               </li>
               <li>
@@ -165,8 +166,9 @@ export default function itemCard() {
                   >
                     <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z" />
                   </svg>
-                  <span className="flex-1 ml-3 whitespace-nowrap">Sub Machine Guns</span>
-              
+                  <span className="flex-1 ml-3 whitespace-nowrap">
+                    Sub Machine Guns
+                  </span>
                 </a>
               </li>
               <li>
@@ -200,9 +202,7 @@ export default function itemCard() {
                   >
                     <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
                   </svg>
-                  <span className="flex-1 ml-3 whitespace-nowrap">
-                    Pistols
-                  </span>
+                  <span className="flex-1 ml-3 whitespace-nowrap">Pistols</span>
                 </a>
               </li>
               <li>
@@ -244,7 +244,9 @@ export default function itemCard() {
                     <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z" />
                     <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z" />
                   </svg>
-                  <span className="flex-1 ml-3 whitespace-nowrap">MarksMan</span>
+                  <span className="flex-1 ml-3 whitespace-nowrap">
+                    MarksMan
+                  </span>
                 </a>
               </li>
             </ul>
@@ -252,10 +254,36 @@ export default function itemCard() {
         </aside>
 
         <div className="p-4 sm:ml-64">
-          <div className="p-4 border-2 border-gray-200 border-solid rounded-lg dark:border-gray-700 mb-2">
+          <div className="p-4 border-2 border-gray-200 border-solid rounded-lg dark:border-gray-700 mb-2 h-full">
             <div className="grid grid-cols-3 gap-4 mb-4">
               {posts.map((item: any) => (
-                <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800 hover:bg-indigo-950" key={item.id}>{item.shortName}</div>
+              
+                  <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" key={item.id}>
+                    <img
+                      className="p-8 rounded-t-lg"
+                      src={item.image512pxLink}
+                      alt={item.shortName}
+                    />
+                    <div className="px-5 pb-5">
+                      <a href="#">
+                        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                          {item.shortName}
+                        </h5>
+                      </a>
+                      <div className="flex items-center justify-between">
+                        <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                          {item.avg24hPrice}
+                        </span>
+                        <a
+                          href="#"
+                          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        >
+                          More Info
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+               
               ))}
             </div>
           </div>
@@ -264,4 +292,3 @@ export default function itemCard() {
     </>
   );
 }
-
