76d825d30ce8ec4ec765ed39cd19ed337ba1e696
alex barlow <alex.barlow@rocketmail.com>
2023-08-16 20:00:29 -0400

    guns with no average sales price are now listed as barter

--- components/itemCard.tsx
+++ components/itemCard.tsx
@@ -82,6 +82,8 @@ export default function itemCard() {
       console.log(traderSell);
     }
   }, [posts]);
+
+
   return (
     <>
       <div className="mt-20">
@@ -257,34 +259,35 @@ export default function itemCard() {
           <div className="p-4 border-2 border-gray-200 border-solid rounded-lg dark:border-gray-700 mb-2 h-full ">
             <div className="grid grid-cols-3 gap-4 mb-4">
               {posts.map((item: any) => (
-              
-                  <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" key={item.id}>
-                    <img
-                      className="p-8 rounded-t-lg"
-                      src={item.image512pxLink}
-                      alt={item.shortName}
-                    />
-                    <div className="px-5 pb-5">
-                      <a href="#">
-                        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                          {item.shortName}
-                        </h5>
+                <div
+                  className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+                  key={item.id}
+                >
+                  <img
+                    className="p-8 rounded-t-lg"
+                    src={item.image512pxLink}
+                    alt={item.shortName}
+                  />
+                  <div className="px-5 pb-5">
+                    <a href="#">
+                      <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                        {item.shortName}
+                      </h5>
+                    </a>
+                    <div className="flex items-center justify-between">
+                      <span className="text-2xl font-bold text-gray-900 dark:text-white">
+                      {item.avg24hPrice != 0 && <h2>₽{item.avg24hPrice}</h2>}
+                      {item.avg24hPrice === 0 && 'Barter'}
+                      </span>
+                      <a
+                        href="#"
+                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                      >
+                        More Info
                       </a>
-                      <div className="flex items-center justify-between">
-                        <span className="text-2xl font-bold text-gray-900 dark:text-white">
-                        ₽{item.avg24hPrice}
-                        </span>
-                        
-                        <a
-                          href="#"
-                          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                        >
-                          More Info
-                        </a>
-                      </div>
                     </div>
                   </div>
-               
+                </div>
               ))}
             </div>
           </div>
